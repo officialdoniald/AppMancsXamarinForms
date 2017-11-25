10e8c21ba4216fa6aa11e2ed0d28bb55b59c368a
Lénárt Bence <bence960206@gmail.com>
2017-11-25 13:07:05 +0100

    signup

--- AppMancsXamarinForms/AppMancsXamarinForms/SignUpPage.xaml.cs
+++ AppMancsXamarinForms/AppMancsXamarinForms/SignUpPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppMancsXamarinForms.BLL.ViewModel;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,35 @@ namespace AppMancsXamarinForms
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SignUpPage : ContentPage
     {
+        SignupPageViewModel signupPageViewModel = new SignupPageViewModel();
+
         public SignUpPage()
         {
             InitializeComponent();
+        }
+
+        private void signupButton_Clicked(object sender, EventArgs e)
+        {
+            User user = new User()
+            {
+                Email = emailEntry.Text.ToLower(),
+                Password = pwEntry.Text,
+                FirstName = firstnameEntry.Text,
+                LastName = lastnameEntry.Text,
+                FacebookId = "",
+                ProfilePictureURL = ""
+            };
+
+            string success = signupPageViewModel.SignUp(user);
+
+            if (!String.IsNullOrEmpty(success))
+            {
+                //HIBA
+            }
+            else
+            {
+                Navigation.PopToRootAsync();
+            }
         }
     }
 }
