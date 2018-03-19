1a556415a2f6ec9ccda47fb666167d6edbc8e78c
officialdoniald <bence960206@gmail.com>
2018-03-19 18:15:25 +0100

    listview transparent selecteditemcolor

---
+++ AppMancsXamarinForms/AppMancsXamarinForms.iOS/CustomRenderers/ExtendedVievCellRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using AppMancsXamarinForms.CustomControls;
+using AppMancsXamarinForms.iOS.CustomRenderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ExtendedViewCell), typeof(ExtendedViewCellRenderer))]
+namespace AppMancsXamarinForms.iOS.CustomRenderers
+{
+    public class ExtendedViewCellRenderer : Xamarin.Forms.Platform.iOS.ViewCellRenderer
+    {
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            var cell = base.GetCell(item, reusableCell, tv);
+            var view = item as ExtendedViewCell;
+            cell.SelectedBackgroundView = new UIView
+            {
+                BackgroundColor = view.SelectedBackgroundColor.ToUIColor(),
+            };
+
+            return cell;
+        }
+
+    }
+}
